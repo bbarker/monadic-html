421269f507c7180e4a89484e4f462d90b7633330
Brandon Elam Barker <beb82@cornell.edu>
2017-08-06 14:51:29 -0400

    fixed infinite recursion issue but todo list not correctly updated still

--- example-todomvc/src/main/scala/mhtml/todo/MhtmlTodo.scala
+++ example-todomvc/src/main/scala/mhtml/todo/MhtmlTodo.scala
@@ -37,12 +37,12 @@ final case class RemovalEvent(todo: Todo) extends TodoEvent
 object MhtmlTodo extends JSApp {
   val allTodosProxy: Var[List[Todo]] = Var(Nil)
 
-  val all       = TodoList("All", "#/", allTodosProxy.dropRepeats)
-  val active    = TodoList("Active", "#/active", allTodosProxy.map(_.filter(!_.completed)).dropRepeats)
+  val all = TodoList("All", "#/", allTodosProxy.dropRepeats)
+  val active = TodoList("Active", "#/active", allTodosProxy.map(_.filter(!_.completed)).dropRepeats)
   val completed = TodoList("Completed", "#/completed", allTodosProxy.map(_.filter(_.completed)).dropRepeats)
   val todoLists = List(all, active, completed)
 
-  val windowHash: Rx[String] = Rx(dom.window.location.hash).merge{
+  val windowHash: Rx[String] = Rx(dom.window.location.hash).merge {
     val updatedHash = Var(dom.window.location.hash)
     dom.window.onhashchange = (ev: Event) => {
       updatedHash := dom.window.location.hash
@@ -54,25 +54,52 @@ object MhtmlTodo extends JSApp {
     todoLists.find(_.hash === hash).getOrElse(all)
   }.dropRepeats
 
-  val todoListComponentsProxy: Var[List[TaggedComponent[Option[TodoEvent], Todo]]] = Var(Nil)
+  //TODO: try something like this for todoListComponents
+  //  val allTodos: Rx[List[Todo]] = anyEvent.foldp(load()) {
+  //    (last, ev) => updateState(last, ev)
+  //  }.dropRepeats
+
+  type TLCmp = TaggedComponent[Option[TodoEvent], Todo]
   val todoListComponents: Rx[List[TaggedComponent[Option[TodoEvent], Todo]]] =
-    (currentTodoList |@| todoListComponentsProxy).map {
-      case (currentTodos: TodoList, currentComps: List[TaggedComponent[Option[TodoEvent], Todo]]) =>
-        currentTodos.items.map(_.map{todo =>
-          val idx = currentComps.indexWhere{cmp => cmp.tag == todo}
-          println(s"currentComps = ${currentComps}")
-          println(s"comp index is $idx") // DEBUG
-          if (idx >= 0) currentComps(idx)
-          else todoListItem(todo)
-        })
+    currentTodoList.foldp(Rx(List[TLCmp]())){(tlCmps: Rx[List[TLCmp]], currentTodoList: TodoList) =>
+      (tlCmps |@| currentTodoList.items).map {
+        case (currentComps: List[TLCmp], currentTodos: List[Todo]) =>
+          currentTodos.map{ todo =>
+            val idx = currentComps.indexWhere { cmp => cmp.tag == todo }
+            println(s"currentComps = ${currentComps}")
+            println(s"comp index is $idx") // DEBUG
+            if (idx >= 0) currentComps(idx)
+            else todoListItem(todo)
+          }
+      }
     }.flatten.dropRepeats
 
-  val todoListComponentsRunner: Rx[Node] = todoListComponentsProxy
-    .imitate(todoListComponents).dropRepeats.map(_ => { <div/> })
+//  val todoListComponentsProxy: Var[List[TaggedComponent[Option[TodoEvent], Todo]]] = Var(Nil)
+//  val todoListComponents: Rx[List[TaggedComponent[Option[TodoEvent], Todo]]] =
+//    (currentTodoList |@| todoListComponentsProxy.dropRepeats).map {
+//      case (currentTodos: TodoList, currentComps: List[TaggedComponent[Option[TodoEvent], Todo]]) =>
+//        currentTodos.items.map(_.map { todo =>
+//          val idx = currentComps.indexWhere { cmp => cmp.tag == todo }
+//          println(s"currentComps = ${currentComps}")
+//          println(s"comp index is $idx") // DEBUG
+//          if (idx >= 0) currentComps(idx)
+//          else todoListItem(todo)
+//        })
+//    }.flatten.dropRepeats
+//
+//  val todoListComponentsRunner: Rx[Node] = todoListComponentsProxy
+//    .imitate(todoListComponents).dropRepeats.map(_ => {
+//      <div/>
+//  })
+
+  //DEBUG
+  todoListComponents.impure.foreach(tlc => println(s"tlc = $tlc"))
+//  todoListComponentsProxy.impure.foreach(tlc => println(s"tlc proxy = $tlc"))
 
   val todoListEvent: Rx[Option[TodoEvent]] = {
     val todoListModelsRx: Rx[List[Rx[Option[TodoEvent]]]] =
-      todoListComponents.map(compList => compList.map(comp => comp.model)).dropRepeats
+      todoListComponents.map(compList => compList.map(comp => comp.model))
+    val cc = todoListModelsRx.impure.foreach{ev => println(s"DEBUG: got a todoListEvent: $ev")}
     todoListModelsRx.flatMap(todoListModels =>
       todoListModels.foldRight[Rx[Option[TodoEvent]]](Rx(None))(
         (lastEv: Rx[Option[TodoEvent]], nextEv: Rx[Option[TodoEvent]]) => nextEv |+| lastEv
@@ -82,6 +109,7 @@ object MhtmlTodo extends JSApp {
 
   val header: Component[Option[AddEvent]] = {
     val newTodo = Var[Option[AddEvent]](None)
+
     def onInputKeydown(event: KeyboardEvent): Unit = {
       (event.currentTarget, event.keyCode) match {
         case (input: HTMLInputElement, KeyCode.Enter) =>
@@ -94,6 +122,7 @@ object MhtmlTodo extends JSApp {
         case _ =>
       }
     }
+
     val headerNode =
       <header class="header">
         <h1>todos</h1>
@@ -112,15 +141,17 @@ object MhtmlTodo extends JSApp {
       completed.items.map(x => if (x.isEmpty) "hidden" else "visible")
     val footerNode =
       <footer class="footer" style:display={display}>
-        <ul class="filters">{todoLists.map(todoListsFooter)}</ul>
+        <ul class="filters">
+          {todoLists.map(todoListsFooter)}
+        </ul>
         <button onclick={() =>
+          println("DEBUG: footer clicked")
           allTodos.map(_.filter(_.completed).foreach(todo =>
             removeTodo := Some(RemovalEvent(todo))
           ))
-          ()
-          }
-          class="clear-completed"
-          style:visibility={visibility}>
+          ()}
+                class="clear-completed"
+                style:visibility={visibility}>
           Clear completed
         </button>
       </footer>
@@ -130,17 +161,23 @@ object MhtmlTodo extends JSApp {
   val anyEvent: Rx[Option[TodoEvent]] =
     (todoListEvent |+| footer.model |+| header.model).dropRepeats
 
+  anyEvent.impure.foreach(ev => println(s"anyEvent: $ev")) //DEBUG
+
   val allTodos: Rx[List[Todo]] = anyEvent.foldp(load()) {
     (last, ev) => updateState(last, ev)
   }.dropRepeats
   val allTodosRunner: Rx[Node] = allTodosProxy.imitate(allTodos)
-    .dropRepeats.map{ _ => { <div/> }}
+    .dropRepeats.map { _ => {
+      <div/>
+  }}
 
   def todoListItem(todo: Todo): TaggedComponent[Option[TodoEvent], Todo] = {
     println(s"making new todo component for $todo") // DEBUG
     val removeTodo = Var[Option[RemovalEvent]](None)
     val updateTodo = Var[Option[UpdateEvent]](None)
+    updateTodo.foreach(ev => println(s"DEBUG: updateTodo $ev"))
     val suppressOnBlur = Var(false)
+
     def submit(event: Event) = {
       suppressOnBlur := true
       editingTodo := None
@@ -151,6 +188,7 @@ object MhtmlTodo extends JSApp {
           updateTodo := Some(UpdateEvent(todo, Todo(trimmedTitle, todo.completed)))
       }
     }
+
     def onEditTodoTitle(event: KeyboardEvent): Unit = {
       event.keyCode match {
         case KeyCode.Escape =>
@@ -162,20 +200,28 @@ object MhtmlTodo extends JSApp {
         case _ =>
       }
     }
+
     def ignoreEvent(event: Event): Unit = ()
+
     def blurHandler: Rx[Event => Unit] =
       suppressOnBlur.map(x => if (x) ignoreEvent(_) else submit(_)).dropRepeats
+
     def onToggleCompleted(event: Event): Unit = {
+      println("DEBUG: onToggleCompleted")
       event.currentTarget match {
         case input: HTMLInputElement =>
-          updateTodo := Some(UpdateEvent(todo, Todo(todo.title, input.checked)))
+          updateTodo := Some(
+            UpdateEvent(todo, Todo(todo.title, input.checked))
+          )
         case _ =>
       }
     }
+
     def onDoubleClick(event: Event): Unit = {
       editingTodo := Some(todo)
       focusInput()
     }
+
     val onDelete: (Event) => Unit = _ =>
       removeTodo := Some(RemovalEvent(todo))
 
@@ -186,32 +232,34 @@ object MhtmlTodo extends JSApp {
     }
     val data: Rx[Option[TodoEvent]] = Semigroup[Rx[Option[TodoEvent]]].combine(removeTodo, updateTodo)
     val todoListElem =
-      <li class={ css }>
+      <li class={css}>
         <div class="view">
-          <input onclick={ onToggleCompleted _ }
+          <input onclick={onToggleCompleted _}
                  class="toggle"
                  type="checkbox"
-                 checked={ todo.completed } />
-          <label ondblclick={ onDoubleClick _ }>{ todo.title }</label>
-          <button onclick={ onDelete } class="destroy"></button>
+                 checked={todo.completed}/>
+          <label ondblclick={onDoubleClick _}>
+            {todo.title}
+          </label>
+          <button onclick={onDelete} class="destroy"></button>
         </div>
-        <input onkeydown={ onEditTodoTitle _ }
+        <input onkeydown={onEditTodoTitle _}
                id="editInput"
                class="edit"
-               value={ todo.title }
-               onblur={ blurHandler }/>
+               value={todo.title}
+               onblur={blurHandler}/>
       </li>
     TaggedComponent(todoListElem, data.dropRepeats, todo)
   }
 
   val todoListElems: Rx[List[Node]] =
-    todoListComponents.map{tlcSeq => tlcSeq.map(comp => comp.view)}.dropRepeats
+    todoListComponents.map { tlcSeq => tlcSeq.map(comp => comp.view) }.dropRepeats
 
   val mainSection: Component[List[UpdateEvent]] = {
     val todoUpdates = Var[List[UpdateEvent]](Nil)
 
     def setAllCompleted(todosIn: Seq[Todo], completed: Boolean): List[UpdateEvent] =
-      todosIn.flatMap{
+      todosIn.flatMap {
         case todo if todo.completed != completed =>
           Some(UpdateEvent(todo, Todo(todo.title, completed)))
         case _ => None
@@ -220,6 +268,7 @@ object MhtmlTodo extends JSApp {
     // TODO(olafur) This is broken in 0.1, fix here https://github.com/OlivierBlanvillain/monadic-html/pull/9
     val checked = active.items.map(x => x.isEmpty)
     val display = allTodos.map(todos => if (todos.isEmpty) "none" else "")
+
     def setAllCompletedHandler(event: Event): Unit = {
       event.currentTarget match {
         case input: HTMLInputElement =>
@@ -229,19 +278,22 @@ object MhtmlTodo extends JSApp {
         case _ => ()
       }
     }
+
     val mainDiv =
-      <section class="main" style:display={ display }>
-        <input onclick={ setAllCompletedHandler _ }
-          type="checkbox"
-          class="toggle-all"
-          checked={ checked } />
-        <label for="toggle-all" checked={ checked }>Mark all as complete</label>
-        <ul class="todo-list">{ todoListElems }</ul>
+      <section class="main" style:display={display}>
+        <input onclick={setAllCompletedHandler _}
+               type="checkbox"
+               class="toggle-all"
+               checked={checked}/>
+        <label for="toggle-all" checked={checked}>Mark all as complete</label>
+        <ul class="todo-list">
+          {todoListElems}
+        </ul>
       </section>
     Component(mainDiv, todoUpdates.dropRepeats)
   }
 
-//  object Model {
+  //  object Model {
   val LocalStorageName = "todo.mhtml"
 
   def load(): List[Todo] =
@@ -253,7 +305,7 @@ object MhtmlTodo extends JSApp {
   val editingTodo: Var[Option[Todo]] = Var[Option[Todo]](None)
 
   def unzipListComponents(listComps: Seq[Component[List[TodoEvent]]])
-    : (List[Node], List[Rx[List[TodoEvent]]])
+  : (List[Node], List[Rx[List[TodoEvent]]])
     = listComps.toList.map(tlc => (tlc.view, tlc.model)).unzip
 
   case class ModelSources(
@@ -261,7 +313,9 @@ object MhtmlTodo extends JSApp {
     changes:  List[TodoEvent]
   )
 
-  def updateState(currentTodos: List[Todo], evOpt: Option[TodoEvent]): List[Todo] = evOpt match {
+  def updateState(currentTodos: List[Todo], evOpt: Option[TodoEvent]): List[Todo] = {
+    println(s"running updateState with event $evOpt on $currentTodos")
+    evOpt match {
     case Some(AddEvent(newTodo)) => {
       println("DEBUG: AddEvent(newTodo)")
       newTodo :: currentTodos
@@ -282,6 +336,7 @@ object MhtmlTodo extends JSApp {
       currentTodos
     }
   }
+}
 
   def focusInput(): Unit =
     dom.document.getElementById("editInput") match {
